7fd5c03098e649d1d05b0ebc04eb8b2805b34208
Thomas CHARMES <charmes.thomas@gmail.com>
2022-11-16 13:34:19 +0100

    Exercice P2C3 Router

---
+++ cafe-with-a-vue/cli-version/src/views/Contact.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+	name: "Contact",
+	data() {
+		return {
+			address: "18 avenue du Beurre, Paris, France",
+			email: "hello@cafewithavue.bakery",
+			phone: "01 88 88 88 88"
+		}
+	}
+}
+</script>
+
+<template>
+	<div>
+		<h2>Contactez nous</h2>
+		<p>Adresse : {{ address }}</p>
+		<p>Téléphone : {{ phone }}</p>
+		<p>Email : {{ email }}</p>
+		<p>Horaires :</p>
+		<ul>
+			<li>L-V: 06:00 à 16:00</li>
+			<li>Samedi: 07:00 à 14:00</li>
+			<li>Dimanche: 07:00 à 12:00</li>
+		</ul>
+	</div>
+</template>
+
+<style></style>
