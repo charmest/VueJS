7e747b35ad38c5bd90e1d49f8fe22b5f8ac3592c
Thomas CHARMES <charmes.thomas@gmail.com>
2022-11-17 18:51:11 +0100

    Exercice P3C1 : Ajout du scss

--- cafe-with-a-vue/cli-version/src/components/MenuItem.vue
+++ cafe-with-a-vue/cli-version/src/components/MenuItem.vue
@@ -1,7 +1,28 @@
 <script>
 export default {
 	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
+
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
 }
 </script>
 
@@ -10,17 +31,32 @@ export default {
 		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
 		<div>
 			<h3>{{ name }}</h3>
+			<p>
+				Prix : {{ generatedPrice }}
+				<span v-if="onSale">(10% de réduction!)</span>
+			</p>
 			<p v-if="inStock">En stock</p>
 			<p v-else>En rupture de stock</p>
 			<div>
 				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
 				<input v-model.number="quantity" id="add-item-quantity" type="number" />
 				<button @click="addToShoppingCart(quantity)">
-					Ajouter au panier d'achat
+					Ajouter au panier
 				</button>
 			</div>
 		</div>
 	</div>
 </template>
 
-<style></style>
+<style lang="scss">
+.menu-item {
+	display: flex;
+	width: 500px;
+	justify-content: space-between;
+	margin-bottom: 30px;
+
+	&__image {
+		max-width: 300px;
+	}
+}
+</style>
--- cafe-with-a-vue/cli-version/src/views/Home.vue
+++ cafe-with-a-vue/cli-version/src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="app" class="app">
+	<div>
 		<h1>{{ restaurantName }}</h1>
 		<p class="description">
 			Bienvenue dans notre café {{ restaurantName }}! Nous sommes réputés pour
@@ -15,15 +15,16 @@
 				:addToShoppingCart="addToShoppingCart"
 				:name="item.name"
 				:image="item.image"
+				:price="item.price"
 				:quantity="item.quantity"
 				:inStock="item.inStock"
 				:key="item.name"
 			/>
 		</section>
 
-		<aside class="shopping-cart">
-			<h2>Panier d'achat : {{ shoppingCart }} articles</h2>
-		</aside>
+		<div class="shopping-cart">
+			<h2>Panier: {{ shoppingCart }} articles</h2>
+		</div>
 
 		<footer class="footer">
 			<p>{{ copyright }}</p>
@@ -41,7 +42,7 @@ export default {
 	},
 	data() {
 		return {
-			restaurantName: "La belle Vue",
+			restaurantName: "La belle vue",
 			shoppingCart: 0,
 			simpleMenu: [
 				{
@@ -51,7 +52,8 @@ export default {
 						alt: "Un croissant"
 					},
 					inStock: true,
-					quantity: 1
+					quantity: 1,
+					price: 2.99
 				},
 				{
 					name: "Baguette de pain",
@@ -60,7 +62,8 @@ export default {
 						alt: "Quatre baguettes de pain"
 					},
 					inStock: true,
-					quantity: 1
+					quantity: 1,
+					price: 3.99
 				},
 				{
 					name: "Éclair",
@@ -69,7 +72,8 @@ export default {
 						alt: "Éclair au chocolat"
 					},
 					inStock: false,
-					quantity: 1
+					quantity: 1,
+					price: 4.99
 				}
 			]
 		}
@@ -88,3 +92,29 @@ export default {
 	}
 }
 </script>
+
+<style lang="scss">
+.description {
+	max-width: 960px;
+	font-size: 1.2rem;
+	margin: 0 auto;
+}
+
+.footer {
+	font-style: italic;
+	text-align: center;
+}
+
+.menu {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+}
+
+.shopping-cart {
+	position: absolute;
+	right: 30px;
+	top: 0;
+}
+</style>
